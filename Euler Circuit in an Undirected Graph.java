c691d6096a6d1eb66ccf89adf173e7b22e388c2a
sushantjoshi800 <79572922+sjoshihypen@users.noreply.github.com>
2024-04-19 09:22:31 +0530

    Update Euler Circuit in an Undirected Graph.java

--- Euler Circuit in an Undirected Graph.java
+++ Euler Circuit in an Undirected Graph.java
@@ -1,3 +1,42 @@
+Euler Circuit in an Undirected Graph
+    
+Eulerian Path is a path in a graph that visits every edge exactly once. 
+Eulerian Circuit is an Eulerian Path that starts and ends on the same vertex. 
+Given the number of vertices v and adjacency list adj denoting the graph. Find that there exists the Euler circuit or not. 
+Return 1 if there exist  alteast one eulerian circuit else 0.
+
+Example 1:
+
+Input: 
+v = 4 
+edges[] = {{0, 1}, {0, 2},{1, 3}, {2, 3}}
+Output: 
+1
+Explanation: corresponding adjacency list will be {{1, 2},{0, 3},{0, 3},{1, 2}}
+One of the Eularian circuit 
+starting from vertex 0 is as follows:
+0->1->3->2->0
+
+Example 2:
+Input: 
+v = 3
+edges[] = {{0, 1},{0, 2}}
+Output: 
+0
+Explanation: The corresponding adjacency list will be {{1, 2}}
+No Eulerian path is found.
+Your Task:
+You don't need to read or print anything. Your task is to complete the function isEularCircuitExist() which
+takes v and array of edges adj[] as input parameter and returns boolean value 1 if Eular circuit exists otherwise returns 0.
+
+Expected Time Complexity: O(v + e)
+Expected Space Complexity: O(v)
+
+Constraints:
+1 <= v <= 10^5
+1 <= edges <= 2*10^5
+    
+Solution 
 class Solution {
     public boolean isEularCircuitExist(int v, ArrayList<ArrayList<Integer>> adj) {
         for (int i = 0; i < v; i++) {
@@ -7,7 +46,6 @@ class Solution {
         }
         return true;
     }
-
 public static void main(String[] args) {
         int v = 5;
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
@@ -17,4 +55,4 @@ public static void main(String[] args) {
     }
 }
 
-// Code Written : Sushant Joshi
+// Code Written: Sushant Joshi
